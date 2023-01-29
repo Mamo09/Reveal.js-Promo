960f689d1565b0be0caf1c9701539de505926e1f
Mamo09 <96432291+Mamo09@users.noreply.github.com>
2023-01-29 22:08:39 +0700

    Mamo

---
+++ style/style.js
@@ -0,0 +1,25 @@
+// Home
+
+
+
+// Produk
+
+
+
+// Fitur
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+        placement : 'top',
+        trigger : 'hover',
+        html : true,
+        content : '<div class="media"><img src="https://img.icons8.com/ios-glyphs/30/null/filled-circle.png" class="mr-3" alt="Sample Image"><div class="media-body"><h5 class="media-heading">Jhon Carter</h5><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
+    });
+});
+
+
+// Spesifikasi
+
+
+
+// Video 
