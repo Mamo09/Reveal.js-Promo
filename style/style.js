c7ae3e277dde70eb6609f7c7f888c36f3d6ccbee
yanxhz <adrianduha123@gmail.com>
2023-01-30 17:55:04 +0700

    Memperbaiki Beberapa Tampilan

--- style/style.js
+++ style/style.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
         placement : 'top',
         trigger : 'hover',
         html : true,
-        content : '<div class="media" ><img src="./image/layar1.png" width="242px" height="160px"><div class="media-body"><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
+        content : '<div class="media" ><img src="./image/layar1.png" width="242px" height="160px"><div class="media-body"><p>Dilengkapi dengan Screen Size 16inci, resolusi 2k Plus, 2560x1600p(16:10)</p></div></div>'
     });
 });
 
@@ -31,7 +31,7 @@ $(document).ready(function(){
         placement : 'top',
         trigger : 'hover',
         html : true,
-        content : '<div class="media" ><img src="./image/layar2.png" width="242px" height="160px"><div class="media-body"><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
+        content : '<div class="media" ><img src="./image/layar2.png" width="242px" height="160px"><div class="media-body"><p>Dilengkapi dengan layar ke 2 yang memiliki Resolusi 4k 3840x1100p, Dan sudah Touch Screen</p></div></div>'
     });
 });
 
@@ -40,7 +40,7 @@ $(document).ready(function(){
         placement : 'top',
         trigger : 'hover',
         html : true,
-        content : '<div class="media" ><img src="./image/touchpad.png" width="242px" height="160px"><div class="media-body"><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
+        content : '<div class="media" ><img src="./image/touchpad.png" width="242px" height="160px"><div class="media-body"><p>Memiliki Touchpad disebelah kanan tanpa ada klik kiri & kanan dan memiliki backlight berwarna merah</p></div></div>'
     });
 });
 
@@ -49,7 +49,7 @@ $(document).ready(function(){
         placement : 'top',
         trigger : 'hover',
         html : true,
-        content : '<div class="media" ><img src="./image/keyboard.png" width="242px" height="160px"><div class="media-body"><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
+        content : '<div class="media" ><img src="./image/keyboard.png" width="242px" height="160px"><div class="media-body"><p>Touchpad dilengkapi dengan Backlight Led bisa dicustomize via aura sync</p></div></div>'
     });
 });
 
@@ -58,7 +58,7 @@ $(document).ready(function(){
         placement : 'top',
         trigger : 'hover',
         html : true,
-        content : '<div class="media" ><img src="./image/kirilaptop.png" width="242px" height="160px" ><div class="media-body"><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
+        content : '<div class="media" ><img src="./image/kirilaptop.png" width="242px" height="160px" ><div class="media-body"><p>Dilengkapi USB TYPE A & C, JACK AUDIO 3-5MM, SD CARD READER dan PORT ADAPTER</p></div></div>'
     });
 });
 // Spesifikasi
