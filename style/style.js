a53211d526f1058178aac80f6d83bbe53291097c
Mamo09 <96432291+Mamo09@users.noreply.github.com>
2023-01-30 15:21:00 +0700

    mamo

--- style/style.js
+++ style/style.js
@@ -9,13 +9,32 @@
 // Fitur
 
 $(document).ready(function(){
-    $('[data-toggle="popover"]').popover({
+    $('[data-toggle="kamera"]').popover({
         placement : 'top',
         trigger : 'hover',
         html : true,
-        content : '<div class="media"><img src="https://img.icons8.com/ios-glyphs/30/null/filled-circle.png" class="mr-3" alt="Sample Image"><div class="media-body"><h5 class="media-heading">Jhon Carter</h5><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
+        content : '<div class="media"><img src="" class="mr-3" alt="Sample Image"><div class="media-body"><h5 class="media-heading">Jhon Carter</h5><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
     });
 });
+
+$(document).ready(function(){
+    $('[data-toggle="layar1"]').popover({
+        placement : 'top',
+        trigger : 'hover',
+        html : true,
+        content : '<div class="media"><img src="" class="mr-3" alt="Sample Image"><div class="media-body"><h5 class="media-heading">Jhon Carter</h5><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
+    });
+});
+
+$(document).ready(function(){
+    $('[data-toggle="layar2"]').popover({
+        placement : 'top',
+        trigger : 'hover',
+        html : true,
+        content : '<div class="media"><img src="" class="mr-3" alt="Sample Image"><div class="media-body"><h5 class="media-heading">Jhon Carter</h5><p>Excellent Bootstrap popover! I really love it.</p></div></div>'
+    });
+});
+
 
 
 // Spesifikasi
